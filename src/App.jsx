81fd1bf1019cfd44b815fdd069d1a18537fde1f8
Anshu Shee <anshu.shee.cg@gmail.com>
2026-03-25 14:33:35 +0530

    Add main App component and README

---
+++ src/App.jsx
@@ -0,0 +1,57 @@
+import { useEffect, useRef } from "react";
+import Hero from "./components/Hero";
+import NavBar from "./components/NavBar";
+import ProductViewer from "./components/ProductViewer";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+import Showcase from "./components/Showcase";
+import Performance from "./components/Performance";
+import Features from "./components/Features";
+import Highlights from "./components/Highlights";
+import Footer from "./components/Footer";
+import Lenis from "lenis";
+
+gsap.registerPlugin(ScrollTrigger);
+
+const App = () => {
+  const lenisRef = useRef(null);
+
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.2,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      smoothWheel: true,
+      touchMultiplier: 1.5,
+    });
+
+    lenisRef.current = lenis;
+
+    // Sync Lenis with GSAP ScrollTrigger
+    lenis.on("scroll", ScrollTrigger.update);
+
+    gsap.ticker.add((time) => {
+      lenis.raf(time * 1000);
+    });
+    gsap.ticker.lagSmoothing(0);
+
+    return () => {
+      gsap.ticker.remove(lenis.raf);
+      lenis.destroy();
+    };
+  }, []);
+
+  return (
+    <main>
+      <NavBar />
+      <Hero />
+      <ProductViewer />
+      <Showcase />
+      <Performance />
+      <Features />
+      <Highlights />
+      <Footer />
+    </main>
+  );
+};
+
+export default App;
